e24b449d10c06dec141e414801002ca109f68fce
Shaun <614027488@qq.com>
2022-01-18 02:12:10 +0800

    Update top_demo.py

--- flask_run/top_demo.py
+++ flask_run/top_demo.py
@@ -6,4 +6,4 @@ def hello_world():
     return 'Hello World! ISDN3002 project!'
 
 if __name__ == '__main__':
-    app.run(host='34.125.172.254',port=80,debug=True)
+    app.run(host='0.0.0.0',port=80,debug=True)
