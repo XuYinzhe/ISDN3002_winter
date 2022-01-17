9d2a8f54cc522dd66dd6ccb69ef7ba9858bcc9be
Shaun <614027488@qq.com>
2022-01-18 01:25:06 +0800

    Create top_demo.py

---
+++ flask_run/top_demo.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World! ISDN3002 project!'
+
+if __name__ == '__main__':
+    app.run(host='34.125.172.254',debug=True)
