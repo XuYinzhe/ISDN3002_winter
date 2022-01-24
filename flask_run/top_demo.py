7b75f3234aa27315339bda029d95ed181fd14062
Shaun <614027488@qq.com>
2022-01-24 18:36:21 +0800

    react-init

--- flask_run/top_demo.py
+++ flask_run/top_demo.py
@@ -6,4 +6,5 @@ def hello_world():
     return 'Hello World! ISDN3002 project!'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=5000,debug=True)
+    #app.run(host='0.0.0.0',port=5000,debug=True)
+    app.run(debug=True)
