aa0fa358fa6127afd2975d151d70f0f35f4a8bab
AliceGuan07 <59173537+AliceGuan07@users.noreply.github.com>
2022-01-24 23:21:47 +0800

    home page elements complete

--- avata/frontend/src/App.js
+++ avata/frontend/src/App.js
@@ -1,35 +1,61 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
-import logo1 from './pages/logo-icon.png';
-import 'antd/dist/antd.css';
-import { Button, Tooltip } from 'antd';
-import { IdcardOutlined } from '@ant-design/icons';
+import { Button, Tooltip, Input, Divider } from 'antd';
+import { IdcardOutlined, SearchOutlined } from '@ant-design/icons';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import navbar from "./components/navbar"
 import login from './pages/login';
 import home from './pages/home'
+import ReactDOM, { render } from 'react-dom';
+import { Components } from 'antd/lib/date-picker/generatePicker';
 
-function App() {
-  return (
-    
-    <div>
-      <header>
-        <img src={logo1} className="App-logo" alt="logo1"/>
-      </header>
-      <Tooltip title="search">
-        <Button shape="circle" icon={<IdcardOutlined />} size="large" />
-      </Tooltip>
-    
-
-    <Router>
-      <navbar />
-      <Routes>
-        <Route path='/home' exact component={home} />
-        <Route path='/login' component={login} />
-      </Routes>
-    </Router>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (  
+      <div>
+        <Input size="large" placeholder="Search room" prefix={<SearchOutlined />} />
+        <Button icon={<IdcardOutlined />} size="large" href="https://www.google.com" />
+        <>
+        <Divider orientation="left" orientationMargin="0">
+        Room List
+        </Divider>
+          <p>LTA</p>
+        <Divider />
+          <p>LTB</p>
+        <Divider />
+          <p>LTC</p>
+        <Divider />
+          <p>LTD</p>
+        <Divider />
+          <p>LTE</p>
+        <Divider />
+          <p>LTF</p>
+          <Divider />
+          <p>LTG</p>
+        <Divider />
+          <p>LTH</p>
+        <Divider />
+          <p>LTI</p>
+        <Divider />
+          <p>LTJ</p>
+        <Divider />
+          <p>LTK</p>
+          <Divider />
+          <p>Room 1006</p>
+        <Divider />
+          <p>Room 2300</p>
+        <Divider />
+          <p>Room 3200</p>
+        <Divider />
+          <p>Room 4002</p>
+        <Divider />
+          <p>Room 4223</p>
+        </>
+      </div>
+    );
+  }
+  
 }
 
-export default App;
+
+export default App
